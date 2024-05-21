33858a99de16858224b428682f0b2f3667f0b5a0
Arvin Kushwaha <arvin.singh.kushwaha@gmail.com>
2024-05-21 11:22:04 +0200

    CUDA support and initial simulator implementation with `std::valarray`
    - Added CMakeLists.txt modifications for future CUDA support
    - Started implementing rudimentary simulation support using
      `std::valarray` in `simulator/include/simulator/core.hpp`.

--- simulator/include/simulator/core.hpp
+++ simulator/include/simulator/core.hpp
@@ -1,5 +1,44 @@
 #pragma once
 
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <valarray>
+
 namespace simulator {
+
+template <typename T>
+struct OscillatorState {
+    std::slice_array<T> positions;
+    std::slice_array<T> velocities;
+    std::slice_array<T> forces;
+};
+
+template <typename T, size_t Rows, size_t Cols = Rows>
+struct SimulatorState {
+    SimulatorState(T spring_constant) : spring_constant(spring_constant), data(0., Rows * Cols) {}
+
+    OscillatorState<T> operator[](size_t i, size_t j) {
+        return {
+            .positions = data[std::slice(index(i, j), 2, 1)],
+            .velocities = data[std::slice(index(i, j) + 2, 2, 1)],
+            .forces = data[std::slice(index(i, j) + 4, 2, 1)],
+        };
+    }
+
+    const size_t rows = Rows, cols = Cols;
+    const size_t n_dim = 2;
+
+    T spring_constant;
+    std::valarray<T> data;
+
+    static inline size_t index(size_t i, size_t j) { return i * Cols + j; }
+    static inline std::array<size_t, 2> index(size_t i) { return std::array<size_t, 2>{i / Cols, i % Cols}; }
+
+    void copy_to(SimulatorState<T, Rows, Cols> &state) const {
+        state.spring_constant = spring_constant;
+        std::copy(std::begin(data), std::end(data), std::begin(state.data));
+    }
+};
 
 } // namespace simulator
--- simulator/src/main.cpp
+++ simulator/src/main.cpp
@@ -1,4 +1,24 @@
 #include "simulator/core.hpp"
+#include <fmt/core.h>
+#include <fmt/ranges.h>
 
+int main() {
+    simulator::SimulatorState<double, 5, 5> simulator(1.0);
+    simulator.data = 1.;
 
-int main() { return 0; }
+    simulator::SimulatorState<double, 5, 5> simulator2(0.);
+
+    fmt::println("{}", simulator.spring_constant);
+    fmt::println("{}", simulator.data);
+    fmt::println("{}", simulator2.spring_constant);
+    fmt::println("{}", simulator2.data);
+
+    simulator.copy_to(simulator2);
+
+    fmt::println("{}", simulator.spring_constant);
+    fmt::println("{}", simulator.data);
+    fmt::println("{}", simulator2.spring_constant);
+    fmt::println("{}", simulator2.data);
+
+    return 0;
+}
